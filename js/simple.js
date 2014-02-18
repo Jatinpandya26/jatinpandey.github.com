16b1a864b0a84049e2f1ea1746e51fcf46574a3f
jatinpandey <jatinpandey5@gmail.com>
2014-02-18 14:05:52 -0600

    Added subtitle and text

--- js/simple.js
+++ js/simple.js
@@ -1,5 +1,6 @@
-simply.title('Enjoy le massage!');
+simply.title('Pebble Vibrator')
+simply.subtitle('Enjoy le massage!');
+simply.text('This app will give you some sexy vibrations')
 setInterval(function() {
     simply.vibe('long');
 }, 500);
-
