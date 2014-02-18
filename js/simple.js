b05c4b4fd89eb3680730b8cf638052b661e951c8
jatinpandey <jatinpandey5@gmail.com>
2014-02-18 14:35:01 -0600

    Made it more descriptive in directions

--- js/simple.js
+++ js/simple.js
@@ -1,6 +1,11 @@
-simply.title('Pebble Vibrator')
-simply.subtitle('Enjoy le massage!');
-simply.text('This app will give you some sexy vibrations')
+simply.title('Pebble Massager');
+simply.subtitle('Keep track of your directions yo');
+simply.on('accelTap', function(e) {
+  subt = 'Your thing moved ' + (e.direction > 0 ? '+' : '-') + e.axis
+  simply.subtitle('Your thing moved ' + (e.direction > 0 ? '+' : '-') + e.axis + '!\n'
+  + 'x = left/right, y = front/back, z = up/down');
+});
 setInterval(function() {
     simply.vibe('long');
 }, 500);
+
