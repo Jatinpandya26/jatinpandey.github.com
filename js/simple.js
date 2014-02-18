85473bf5ce49ca937c426d0069e18f79b9b291a9
jatinpandey <jatinpandey5@gmail.com>
2014-02-18 14:38:45 -0600

    Simpler

--- js/simple.js
+++ js/simple.js
@@ -1,11 +1,7 @@
-simply.title('Pebble Massager');
-simply.subtitle('Keep track of your directions yo');
 simply.on('accelTap', function(e) {
   subt = 'Your thing moved ' + (e.direction > 0 ? '+' : '-') + e.axis
-  simply.subtitle('Your thing moved ' + (e.direction > 0 ? '+' : '-') + e.axis + '!\n'
-  + 'x = left/right, y = front/back, z = up/down');
+  simply.subtitle(subt);
 });
 setInterval(function() {
-    simply.vibe('long');
-}, 500);
-
+    simply.vibe('short');
+}, 2000);
